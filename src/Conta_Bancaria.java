b4c862d0660f3c257458377ab148ba6be12f6612
Alef Luciano <alef.luciano@gmail.com>
2025-09-27 16:45:45 -0300

    Menu de operações

--- src/Conta_Bancaria.java
+++ src/Conta_Bancaria.java
@@ -8,6 +8,7 @@ public class Conta_Bancaria {
         String cliente = "João";
         String tipoConta = "Corrente";
         double saldo = 2500.00;
+        int opcao = 0;
 
         // Informações do cliente
 
@@ -18,5 +19,25 @@ public class Conta_Bancaria {
         System.out.println("Saldo atual: " + saldo);
         System.out.println("\n*************************");
         System.out.println();
+
+        // Menu de operações
+
+        String operacoes = """
+                ** Operações disponíveis **
+                1 - Consultar saldo
+                2 - Transferir valor
+                3 - Receber valor
+                4 - Sair
+
+               """;
+
+        // Lógica e leitura de dados
+
+        Scanner ler = new Scanner(System.in);
+
+        while (opcao != 4) {
+            System.out.println(operacoes);
+            opcao = ler.nextInt();
+        }
     }
 }
