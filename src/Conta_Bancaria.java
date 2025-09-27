0390bf1a3dc2637cf35c24a28439075d3f7087c2
Alef Luciano <alef.luciano@gmail.com>
2025-09-27 16:40:00 -0300

    Informações do cliente

---
+++ src/Conta_Bancaria.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Conta_Bancaria {
+    public static void main(String[] args) {
+
+        // Declaração ee variáveis
+
+        String cliente = "João";
+        String tipoConta = "Corrente";
+        double saldo = 2500.00;
+
+        // Informações do cliente
+
+        System.out.println();
+        System.out.println("*************************");
+        System.out.println("\nNome do cliente: " + cliente);
+        System.out.println("Tipo de Conta: " + tipoConta);
+        System.out.println("Saldo atual: " + saldo);
+        System.out.println("\n*************************");
+        System.out.println();
+    }
+}
